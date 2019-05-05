1bbbc94d00967a29a8a08dd405df1255559b41d4
Shesh <sheshbhushansnar@gmail.com>
2019-05-05 12:01:27 +1000

    Updated index
    This is it

--- index.php
+++ index.php
@@ -1,194 +1,74 @@
+
 <?php
 //must appear BEFORE the <html> tag
 session_start();
-include_once('include/config.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<?php
+$conn = mysqli_connect("localhost","root","","quiz");
+$query = "SELECT * from test";	
+$result = mysqli_query($conn,$query);
+
+while($row = mysqli_fetch_array($result)):
+?>
+
+
+<html>
+<title>QUIZ</title>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/member.css">
-    <link rel="stylesheet" href="css/responsive.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script src="js/nav.js"></script>
-    <script src="js/read_more.js"></script>
-    <title>World Of Soccer</title>
-</head>
-<body onLoad="run_first()">
-	<?php include("include/banner.inc") ?>
-    <?php include("include/nav.inc") ?>
-    <div class="container-fluid">
-    <br>
-    <h1 style="color:white; text-align:center;">England-Premier League</h1>
-    	<div class="row">
-        	<div class="col">
-
-					<div id="news" class="bg-light mt-3 px-1" style="border-radius: 5px; border:black solid thick; height:680px; max-height:100%; width:auto; background-color:transparent; background: rgba(0, 0, 0, 0.6); border-radius:9px;">
-                    <marquee id="slide" behavior="scroll" direction="up" scrollamount="15" overflow="hidden" onmouseover="this.stop();" onmouseout="this.start();">
-                    <img src="images/richarlison-everton-wolves.jpg" class="img-fluid" alt="Picture" style="width:100%;">
-
-                <h2>Richarlison return a welcome one for Everton</h2>
-				<br><p>Everton have endured mixed fortunes in Marco Silva’s first five Premier League games in charge. However, one stark stat stands is the fact the Toffees have won just one of their first league games.
-In truth, the Toffees were handed a relatively kind start to the season. However, things have gone wrong since the second half at Bournemouth in their third top-flight game of the season.
-The Toffees threw away a two-goal lead, centre-back Michael Keane picked up a head injury, and maybe most crucially their early season star Richarlison was dismissed for moving his head towards Adam Smith.
-It was a petulant act from a very talented young player. No doubt he will have learnt a lot from the incident. The youngster has now served his three-game ban and is available for Sunday’s trip to Arsenal. </p>
-
-					<br><img src="images/Shaqiri-Mane.jpg" class="img-fluid" alt="Picture" style="width:100%;">
-
-                <br> <h2>Xherdan Shaqiri stars in 3-0 win over Southampton that maintains Reds' perfect start to the season</h2>
-				<br><p style="border-bottom:black solid thick;">Liverpool earned their sixth win in a row from the season’s start with a comfortable 3-0 victory over Southampton.
-
-Xherdan Shaqiri celebrated a first Premier League start for Liverpool by having a role in two of their three first-half goals.
-
-On 10 minutes his shot was deflected in off Wesley Hoedt for an own goal.
-
-On 21 minutes, Joel Matip’s towering header from a corner made it 2-0.
-
-After going close a number of times, Mohamed Salah scored on the stroke of half-time, tapping in after Shaqiri’s free-kick had hit the bar.
-
-The second half was quieter. Virgil Van Dijk went off with an injury that did not look serious, while Salah thought he had a late second but was ruled offside.
-
-Liverpool are the top with 18 points, while Southampton are 14th with five.</p>
-
-
-					<br><img src="images/edenhazard0810.jpg" class="img-fluid" alt="Picture" style="width:100%;">
-
-                      <br> <h2>Midweek rest boosts Chelsea winger's potential but it is Mohamed Salah who has the captaincy for Gameweek 6</h2>
-				<br><p style="border-bottom:black solid thick;">Eden Hazard's (£10.9m) outstanding start to 2018/19 ensures his place in The Scout's XI for Gameweek 6.
-
-He is the only player to produce goal or an assist in all five Gameweeks.
-
-Hazard's hat-trick against Cardiff City helped him soar to the top of the FPL player standings on 47 points.
-
-He takes his place in The Scout's 3-5-2 formation as Chelsea travel to West Ham United.
-
-Joe Hart (BUR) £4.5m
-He offers the security of save points should Burnley concede at home to AFC Bournemouth. Hart has only one clean sheet in five Gameweeks but is the third top scoring goalkeeper, having made 26 saves, more than any rival.
-
-Andrew Robertson (LIV) £6.2m
-Liverpool's attacking left-back has already supplied two assists to go with his three clean sheets. Robertson has totalled 20 points in his two home appearances ahead of an Anfield encounter with Southampton.</p>
-
-
-
-				<br><img src="images/1037821302.jpg" class="img-fluid" alt="Picture" style="width:100%;">
-
-                <br><h2>Harry Kane penalty and Erik Lamela strike earns Londoners 2-1 win at Brighton</h2>
-				<br><p style="border-bottom:black solid thick;">Mauricio's reaction External Link
-Erik Lamela came off the bench to give Tottenham Hotspur a 2-1 victory at Brighton & Hove Albion.
-
-Spurs took the lead when Harry Kane netted with an emphatic penalty on 42 minutes after Glenn Murray had handled a Kieran Trippier free-kick.
-
-Brighton improved in the second half and could have equalised on 66 minutes, but Anthony Knockaert shot straight at Paulo Gazzaniga from close range.
-
-Ten minutes later substitute Lamela doubled Spurs' lead from a Danny Rose cut-back.
-
-Knockaert pulled a goal back in the third minute of stoppage time and had a good chance for an equaliser but his weak shot was saved.
-
-Spurs move up to fifth, on 12 points, with Brighton in 13th on five.</p>
-
-
-
-            	<br><img src="images/Granit-Xhaka.jpg" class="img-fluid" alt="Picture" style="width:100%;">
-
-                <br><h2>Adrian Clarke highlights importance of midfielder to Arsenal head coach's game plan</h2>
-				<br><p style="border-bottom:black solid thick;">Adrian Clarke looks at tactical battles set to take place in Matchweek 6.
-
-Arsenal v Everton
-Granit Xhaka is set to play his 100th match for Arsenal on Sunday and yet he still divides opinion among the club's fans.
-
-But his role is integral to the way new Arsenal head coach Unai Emery wants his side to perform.
-
-Emery is keen for his team to play out from the back and then pass through the thirds with speed and efficiency. The Spaniard has quickly recognised the importance of Xhaka’s precise distribution to this objective.
-
-Xhaka has been selected to start all five Premier League matches so far and Emery has handed him more minutes than any other Arsenal midfielder.
-
-Positive passing
-Arsenal’s new man in charge is happy to see patience in possession, but where possible he encourages early passes into forward players.
-
-Pierre-Emerick Aubameyang, Mesut Ozil and Alexandre Lacazette all thrive off instant service that unsettles their markers. In this regard Xhaka is the main man.
-
-He has found Aubameyang and Ozil with 19 and 28 passes respectively, more than any team-mate.
-
-In fact, no one else at Arsenal comes close to making as many vertical passes that dissect the lines as the Swiss does.</p>
-
-
-</marquee>
-        		</div>
-           		</div>
-
-
-
-
-            <div class="col">
-          		<div class="bg-light mt-3 px-2" style="border-radius: 5px; border:black solid thick;">
-<?php $html = ""; $url = "http://www.soccerstats247.com/RankingswFormRssWidget.aspx?langId=1&leagueId=1204&groupName=";$xml = simplexml_load_file($url); foreach($xml->channel->item as $item){$html .= $item->description;$html .= "<hr />";}echo $html; ?>
-</div>
-        		</div>
-
-
-
-
-
-
-  </div>
-
-
-    <p style="border-top:black solid thin;"></p>
-    <h1 style="color:white; margin-top:1%; text-align:center;" >Spain-LaLiga</h1><br>
-
-        	<div class="row">
-
-
-                    	<div class="col">
-
-        		<div class="bg-light mt-3 px-2" style="border-radius: 5px; border:black solid thick;">
-
-<?php $html = ""; $url = "http://www.soccerstats247.com/RankingswFormRssWidget.aspx?langId=1&leagueId=1399&groupName=";$xml = simplexml_load_file($url); foreach($xml->channel->item as $item){$html .= $item->description;$html .= "<hr />";}echo $html; ?></div>
-        		</div>
-
-
-
-                	<div class="col">
-
-                    		<div class="bg-light mt-3 px-2" style=" border:black solid thick; height:680px; width:100%; max-height:100%">
-                            <marquee id="slide" behavior="scroll" direction="up" scrollamount="15" overflow="hidden" onmouseover="this.stop();" onmouseout="this.start();">
-                            <img src="images/skysports-marco-asensio-real-madrid_4430454.jpg" class="img-fluid" alt="Picture" style="width:100%;">
-                <br><h2>Real Madrid 1-0 Espanyol: Marco Asensio scores only goal in victory</h2>
-				<br><p style="border-bottom:black solid thick;"><b>Asensio's low angled finish shortly before half-time sealed Real a third straight home win, but they were far from convincing against dogged opponents.</b>
-
-Julen Lopetegui's side dismantled Champions League rivals Roma at the Bernabeu in midweek, but lacked the same fluency against Espanyol, for whom Borja Iglesias struck the crossbar in the second half. </p><br>
-
-                                            <img src="images/skysports-lionel-messi-barcelona_4425829.jpg" class="img-fluid" alt="Picture" style="width:100%;">
-                <br><h2>Lionel Messi 'makes the extraordinary routine', says Barcelona manager Ernesto Valverde</h2>
-				<br><p style="border-bottom:black solid thick;"><b>Ernesto Valverde hailed the performance of Lionel Messi after his hat-trick helped Barcelona to a 4-0 win against PSV Eindhoven in the Champions League.</b>
-
-Messi scored a sublime free-kick in the first half before two clinical finishes towards the end of the game secured Barca a comfortable opening win in Group B.
-
-It was Messi's eighth Champions League hat-trick - a new record - and Valverde says that despite the feat, it is all in a day's work for his captain.</p><br>
-
-
-                            <img src="images/skysports-valverde-barcelona_4430131.jpg" class="img-fluid" alt="Picture" style="width:100%;" >
-                <br><h2>Ernesto Valverde unsure over committing to Barcelona future</h2>
-				<br><p style="border-bottom:black solid thick;"><b>Barcelona coach Ernesto Valverde has admitted he is not sure if he will still be in charge of the team next season.</b>
-Valverde's contract with Barca expires at the end of the current campaign and he revealed on Saturday he has an option to extend the deal by another year.
-
-"I have a contract and I have a possibility to extend it a year more," said Valerde, who took over the reins from Luis Enrique at the Nou Camp in 2017.</p>
-
-
-                 </marquee>
-                </div>
-           		</div>
-
-            </div>
-
-
-  </div>
-
-
-	<?php include("include/footer.inc") ?>
+<body>
+					
+					
+					<div>
+					<form method='post' action ='index.php'>
+					<table>
+					
+					<tr>
+					<td><?php echo $row['id'];?></td>
+
+					<td><?php echo $row['question'];?></td></tr>
+					<?php $option1= $row['option1'];?>
+					<?php $option2= $row['option2'];?>
+					<?php $option3= $row['option3'];?>
+					<?php $option4= $row['option4'];?>
+					<?php $correct_answer= $row['answer'];?>
+
+					<tr><td><?php echo "<span><input type='radio'name='selectedanswer'>".$option1.""?></span></td>
+					<tr><td><?php echo "<span><input type='radio'name='selectedanswer'>".$option2.""?></span></td>
+					<tr><td><?php echo "<span><input type='radio'name='selectedanswer'>".$option3.""?></span></td>
+					<tr><td><?php echo "<span><input type='radio'name='selectedanswer'>".$option4.""?></span></td>
+					
+
+					<tr><td></td>
+					</tr>
+					
+					</table>
+					
+					
+                    </div>
+			<?php endwhile;?>
+<tr><td><?php echo "<span><input type='submit'name='submit' onClick=='result()'>"?></span></td></form>
 </body>
 </html>
+	
+	
+<script>
+function result()
+{
+$Counter = 0;
+$correct_answer= $row['answer'];
+$selectedanswer = "";
+if(isset($_POST["selectedanswer"]));
+{}
+
+$selectedanswer = $_POST["selectedanswer"];
+if($selectedanswer == $correct_answer)
+{
+
+}
+else {
+echo"counter is not".$counter."";
+	
+}
+
+}
+</script>
