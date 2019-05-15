9623ee33c6ee20121e0feaec147171a26811382f
sheshsonar <38877666+sheshsonar@users.noreply.github.com>
2019-05-15 19:48:55 +1000

    Baisc structure
    basic structure is done

--- test.php
+++ test.php
@@ -1,38 +1,61 @@
-<?php
-//must appear BEFORE the <html> tag
-session_start();
-include_once('include/database.php');
-?>
+<!DOCTYPE html>
 
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-  </head>
-  <body>
+<?php require 'database.php';
+session_start();
+//echo "successful";?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+   
+    <title>About us</title>
+</head>
+<body onLoad="run_first()">
+	<?php include("include/banner.inc") ?>
+    <?php include("include/nav.inc") ?>
+    
+    <div class="container-fluid">
+    	<div class="row">
+        	<div class="col">
+            	
+            		<h2> THIS IS TEST PAGE</h2>
+            
+
+    
+
+
+
+
 
 
 
 <?php
     function getQuestion()
-    {     require 'include/database.php';
+    {     require 'database.php';
           $fetchqry = "SELECT * FROM `question`";
           $result=mysqli_query($con,$fetchqry);
           $num=mysqli_num_rows($result);
           while ($row = mysqli_fetch_assoc($result))
             {
-          $question = array($row['que_id'], $row['que'], $row['option 1'], $row['option 2'], $row['option 3'], $row['option 4'], $row['ans']);
-          $ans_array = array($row['option 1'], $row['option 2'], $row['option 3'], $row['option 4']);
-            shuffle($ans_array);
-            ?>
+              
+
+              $question = array($row['que_id'], $row['que'], $row['option 1'], $row['option 2'], $row['option 3'], $row['option 4'], $row['ans']);
+              $ans_array = array($row['option 1'], $row['option 2'], $row['option 3'], $row['option 4']);
+                shuffle($ans_array);
+                ?>
 
             <form class="" method="post">
               <table align="center">
 
                     <tr><td><?php echo $row['que_id'];?>.<?php echo $row['que']; ?></tr>
 
-                    <tr><td><input required type="radio" name="userans" value="<?=$ans_array[0]?>" required> &nbsp;<?=$ans_array[0]?><br>
+                    <tr><td><input required type="radio" name="userans<?=$row['que_id']?>" value="<?=$ans_array[0]?>" required> &nbsp;<?=$ans_array[0]?><br>
                     <tr><td><input required type="radio" name="userans<?=$row['que_id']?>" value="<?=$ans_array[1]?>">&nbsp;<?=$ans_array[1]?></td></tr>
                     <tr><td><input required type="radio" name="userans<?=$row['que_id']?>" value="<?=$ans_array[2]?>">&nbsp;<?=$ans_array[2]?></td></tr>
                     <tr><td><input required type="radio" name="userans<?=$row['que_id']?>" value="<?=$ans_array[3]?>">&nbsp;<?=$ans_array[3]?><br><br><br></td></tr>
@@ -43,6 +66,10 @@ include_once('include/database.php');
               </table>
             </form>
 
+        	
+            </div>
+        </div>
+    </div>
 
 
 <script>
@@ -72,6 +99,6 @@ include_once('include/database.php');
               ?>
 
 </script>
-
+	<?php include("include/footer.inc") ?>
   </body>
 </html>
