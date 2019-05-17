28a5c4c7f5ab45daa29df59bb6ff768486c4c32d
sheshsonar <38877666+sheshsonar@users.noreply.github.com>
2019-05-17 14:13:52 +1000

    Version 2
    Site structure

--- courses.php
+++ courses.php
@@ -9,12 +9,15 @@ include_once('include/database.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/responsive.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-   
-    <title>About us</title>
+    <script src="js/nav.js"></script>
+    <script src="js/read_more.js"></script>
+    
+    <title>Courses </title>
 </head>
 <body onLoad="run_first()">
 	<?php include("include/banner.inc") ?>
