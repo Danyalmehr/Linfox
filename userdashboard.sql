d9f5df25177c6855a598f0f07017437b57ab6f00
Danyalmehr <38877684+Danyalmehr@users.noreply.github.com>
2019-05-22 02:15:12 +1000

    updated database(question table fixed)

--- userdashboard.sql
+++ userdashboard.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: May 15, 2019 at 02:52 AM
--- Server version: 5.7.24
--- PHP Version: 7.2.14
+-- Host: 127.0.0.1
+-- Generation Time: May 21, 2019 at 06:12 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,13 +28,10 @@ SET time_zone = "+00:00";
 -- Table structure for table `attempt`
 --
 
-DROP TABLE IF EXISTS `attempt`;
-CREATE TABLE IF NOT EXISTS `attempt` (
-  `att_id` int(255) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `attempt` (
+  `att_id` int(255) NOT NULL,
   `final_score` varchar(255) DEFAULT NULL,
-  `test_id` int(255) DEFAULT NULL,
-  PRIMARY KEY (`att_id`),
-  KEY `test_id` (`test_id`)
+  `test_id` int(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -43,12 +40,10 @@ CREATE TABLE IF NOT EXISTS `attempt` (
 -- Table structure for table `courses`
 --
 
-DROP TABLE IF EXISTS `courses`;
-CREATE TABLE IF NOT EXISTS `courses` (
+CREATE TABLE `courses` (
   `course_id` int(255) NOT NULL,
   `course_name` varchar(255) NOT NULL,
-  `course_desc` text NOT NULL,
-  PRIMARY KEY (`course_id`)
+  `course_desc` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -57,32 +52,27 @@ CREATE TABLE IF NOT EXISTS `courses` (
 -- Table structure for table `question`
 --
 
-DROP TABLE IF EXISTS `question`;
-CREATE TABLE IF NOT EXISTS `question` (
-  `que_id` int(255) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `question` (
+  `que_id` int(255) NOT NULL,
   `que` text NOT NULL,
   `option 1` varchar(222) NOT NULL,
   `option 2` varchar(222) NOT NULL,
   `option 3` varchar(222) NOT NULL,
   `option 4` varchar(222) NOT NULL,
   `ans` varchar(222) NOT NULL,
-  `ans_id` int(255) DEFAULT NULL,
-  `test_id` int(255) DEFAULT NULL,
-  PRIMARY KEY (`que_id`),
-  KEY `ansID_fk` (`ans_id`),
-  KEY `test_id` (`test_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `test_id` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `question`
 --
 
-INSERT INTO `question` (`que_id`, `que`, `option 1`, `option 2`, `option 3`, `option 4`, `ans`, `ans_id`, `test_id`) VALUES
-(1, 'What does PHP stand for?', 'Preprocessed Hypertext Page', 'Hypertext Markup Language', 'Hypertext Preprocessor', 'Hypertext Transfer Protocol', 'Hypertext Preprocessor', NULL, NULL),
-(2, 'What will be the value of $var? ', '0', '1', '2', 'NULL', '0', NULL, NULL),
-(3, ' ____________ function in PHP Returns a list of response headers sent (or ready to send)', 'header', 'headers_list', 'header_sent', 'header_send', 'headers_list', NULL, NULL),
-(4, 'Which of the following is the Server Side Scripting Language?', 'HTML', 'CSS', 'JS', 'PHP', 'PHP', NULL, NULL),
-(5, 'From which website you download this source code?', 'Softglobe.net', 'w3school.com', 'technopoints.co.in', 'php.net', 'technopoints.co.in', NULL, NULL);
+INSERT INTO `question` (`que_id`, `que`, `option 1`, `option 2`, `option 3`, `option 4`, `ans`, `test_id`) VALUES
+(1, 'What does PHP stand for?', 'Preprocessed Hypertext Page', 'Hypertext Markup Language', 'Hypertext Preprocessor', 'Hypertext Transfer Protocol', 'Hypertext Preprocessor', NULL),
+(2, 'What will be the value of $var? ', '0', '1', '2', 'NULL', '0', NULL),
+(3, ' ____________ function in PHP Returns a list of response headers sent (or ready to send)', 'header', 'headers_list', 'header_sent', 'header_send', 'headers_list', NULL),
+(4, 'Which of the following is the Server Side Scripting Language?', 'HTML', 'CSS', 'JS', 'PHP', 'PHP', NULL),
+(5, 'From which website you download this source code?', 'Softglobe.net', 'w3school.com', 'technopoints.co.in', 'php.net', 'technopoints.co.in', NULL);
 
 -- --------------------------------------------------------
 
@@ -90,24 +80,42 @@ INSERT INTO `question` (`que_id`, `que`, `option 1`, `option 2`, `option 3`, `op
 -- Table structure for table `result`
 --
 
-DROP TABLE IF EXISTS `result`;
-CREATE TABLE IF NOT EXISTS `result` (
-  `ans_id` int(255) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `result` (
+  `ans_id` int(255) NOT NULL,
   `userans` varchar(255) NOT NULL,
-  `que_id` int(255) DEFAULT NULL,
-  PRIMARY KEY (`ans_id`),
-  KEY `que_id` (`que_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=latin1;
+  `que_id` int(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `result`
 --
 
 INSERT INTO `result` (`ans_id`, `userans`, `que_id`) VALUES
-(65, 'Preprocessed Hypertext Page', 2),
-(66, '0', 4),
-(68, 'header', 5),
-(70, 'Preprocessed Hypertext Page', 2);
+(472, 'Preprocessed Hypertext Page', 1),
+(473, '2', 2),
+(474, 'header', 3),
+(475, 'CSS', 4),
+(476, 'Softglobe.net', 5),
+(477, 'Hypertext Preprocessor', 1),
+(478, '0', 2),
+(479, 'headers_list', 3),
+(480, 'JS', 4),
+(481, 'php.net', 5),
+(482, 'Hypertext Preprocessor', 1),
+(483, '1', 2),
+(484, 'header_send', 3),
+(485, 'JS', 4),
+(486, 'technopoints.co.in', 5),
+(487, 'Preprocessed Hypertext Page', 1),
+(488, '0', 2),
+(489, 'header_sent', 3),
+(490, 'JS', 4),
+(491, 'technopoints.co.in', 5),
+(492, 'Preprocessed Hypertext Page', 1),
+(493, '0', 2),
+(494, 'header_sent', 3),
+(495, 'JS', 4),
+(496, 'technopoints.co.in', 5);
 
 -- --------------------------------------------------------
 
@@ -115,14 +123,77 @@ INSERT INTO `result` (`ans_id`, `userans`, `que_id`) VALUES
 -- Table structure for table `test`
 --
 
-DROP TABLE IF EXISTS `test`;
-CREATE TABLE IF NOT EXISTS `test` (
-  `test_id` int(255) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `test` (
+  `test_id` int(255) NOT NULL,
   `test_name` varchar(255) NOT NULL,
-  `course_id` int(255) DEFAULT NULL,
-  PRIMARY KEY (`test_id`),
-  KEY `course_id` (`course_id`)
+  `course_id` int(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attempt`
+--
+ALTER TABLE `attempt`
+  ADD PRIMARY KEY (`att_id`),
+  ADD KEY `test_id` (`test_id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`que_id`),
+  ADD KEY `test_id` (`test_id`);
+
+--
+-- Indexes for table `result`
+--
+ALTER TABLE `result`
+  ADD PRIMARY KEY (`ans_id`),
+  ADD KEY `que_id` (`que_id`);
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`test_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attempt`
+--
+ALTER TABLE `attempt`
+  MODIFY `att_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `question`
+--
+ALTER TABLE `question`
+  MODIFY `que_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `result`
+--
+ALTER TABLE `result`
+  MODIFY `ans_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=497;
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `test_id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -138,7 +209,6 @@ ALTER TABLE `attempt`
 -- Constraints for table `question`
 --
 ALTER TABLE `question`
-  ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`ans_id`) REFERENCES `result` (`ans_id`),
   ADD CONSTRAINT `question_ibfk_2` FOREIGN KEY (`test_id`) REFERENCES `test` (`test_id`);
 
 --
