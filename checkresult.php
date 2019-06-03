510583bee1ab20a667967ea1ef0513ec036f521a
Danyalmehr <38877684+Danyalmehr@users.noreply.github.com>
2019-06-03 19:37:21 +1000

    Check result and Stest

--- checkresult.php
+++ checkresult.php
@@ -1,7 +1,7 @@
 <?php
-//must appear BEFORE the <html> tag
+require 'database.php';
 session_start();
-include_once('include/database.php');?>
+//echo "successful";?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -41,7 +41,7 @@ include_once('include/database.php');?>
 					//print_r($selected); check to see weather it is retriving the value that user have selected
 
 					$fetchqry = "SELECT * FROM question";
-					$result = mysqli_query($conn,$fetchqry);
+					$result = mysqli_query($con,$fetchqry);
           $num=mysqli_num_rows($result);
 				  $row = mysqli_fetch_array($result);
 
@@ -53,7 +53,7 @@ include_once('include/database.php');?>
             //( '$value', (SELECT `que_id` from `question`) )";
 
             $fetchqry2 = "INSERT INTO useranswer(`userans`, `que_id`) values ('$value','$que_id')";
-            $result2 = mysqli_query($conn,$fetchqry2);
+            $result2 = mysqli_query($con,$fetchqry2);
             $que_id += 1;
           }
           if ($result2) {
@@ -101,7 +101,7 @@ include_once('include/database.php');?>
 
             <p> <span style="background-color: #ADFFB4"><?= $array1[$x] ?></span> </p>
             <?php $score = $score + 1;
-            $scorePercentage = ($score/$count)*100;?>
+              ?>
 
             <?php  }
 
@@ -111,11 +111,13 @@ include_once('include/database.php');?>
       <?php }
 
           $fetchqry3 = "INSERT INTO attempt (`final_score`) values ('$score')";
-          $result3 = mysqli_query($conn,$fetchqry3);
+          $result3 = mysqli_query($con,$fetchqry3);
 
-         <h2> Your have answered <?=$scorePercentage?>% of all the questions </h2>
+          $scorePercentage = ($score/$count)*100;
 
-        <?php  mysqli_free_result($result);?>
+          echo (" <h2>Your have answered ".$scorePercentage."% of all the questions </h2>");
+
+          mysqli_free_result($result);?>
 
           <form>
             <input type="button" class="button" name="back" style="vertical-align:middle" value="Take the test again" onclick="history.go(-1)">
