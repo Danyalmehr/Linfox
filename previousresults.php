830be54d2ce7ded63842054042441043417d5b07
sheshsonar <38877666+sheshsonar@users.noreply.github.com>
2019-06-04 19:15:17 +1000

    Update previousresults.php

--- previousresults.php
+++ previousresults.php
@@ -10,30 +10,32 @@ include_once('include/database.php');
       <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/responsive.css">
+      <link rel="stylesheet" href="css/test.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <script src="js/nav.js"></script>
     <script src="js/read_more.js"></script>
-	
+
 	 <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
         <link type="text/css" href="css/theme.css" rel="stylesheet">
         <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
         <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
-    
+
     <title>Previous Results</title>
 </head>
 <body onLoad="run_first()">
 	<?php include("include/banner.inc") ?>
     <?php include("include/nav.inc") ?>
-    
+
     <div class="container-fluid">
     	<div class="row">
-        	<div class="col">
-				
+        <div class="col-md-3">
 				<div class="span3">
+
+
                         <div class="sidebar" style="display: inline">
                             <ul class="widget widget-menu unstyled">
                                 <li class="left_icon"><a href="dashboard1.php"><i class="menu-icon icon-dashboard"></i>Dashboard
@@ -72,15 +74,17 @@ include_once('include/database.php');
                                 </li>
                                 <li><a href="#"><i class="menu-icon icon-signout"></i>Logout </a></li>
                             </ul>-->
-                        </div>
-            	
-            		<h2> THIS IS previous results PAGE</h2>
-            
-
-            	
             </div>
         </div>
+
     </div>
+
+    <div class="col-md-9">
+      <center class="result_display"><h1>Previous test results</h1><Center>
+
+    </div>
+    </div>
+        </div>
 	<?php include("include/footer.inc") ?>
 </body>
 </html>
