69af0d400bac1b1a94c5232c65b8e455cf2d6efa
sheshsonar <38877666+sheshsonar@users.noreply.github.com>
2019-06-04 23:49:00 +1000

    Tried to make bit neat and clean
    sidebar and test is good now.. checkresult and sidebar is good now

--- Stest.php
+++ Stest.php
@@ -19,8 +19,7 @@ include_once('database.php');
       <link type="text/css" href="css/theme.css" rel="stylesheet">
     <script src="js/nav.js"></script>
     <script src="js/read_more.js"></script>
-	
-		<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
         <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
         <link type="text/css" href="css/theme.css" rel="stylesheet">
         <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
@@ -31,64 +30,80 @@ include_once('database.php');
     <link type="text/css" href="css/theme.css" rel="stylesheet">
 
     <title> Take test </title>
-	
+
 	<style>
+
+  ul.unstyled, ol.unstyled {
+     margin-left: 0;
+     list-style: none;
+}
 		.span3 {
-			
+
 			margin-right: 4em;
 		}
-	
+    .widget-menu {
+    background: #fff;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+     border-radius: 3px;
+    overflow: hidden;
+}
+
 	</style>
 </head>
 <body onLoad="run_first()">
 	<?php include("include/banner.inc") ?>
     <?php include("include/nav.inc") ?>
 
-	
-	 <div class="span3">
-                        <div class="sidebar" style="display: inline">
-                            <ul class="widget widget-menu unstyled">
-                                <li class="left_icon"><a href="dashboard1.php"><i class="menu-icon icon-dashboard"></i>Dashboard
-                                </a></li>
-                                <!--<li><a href="activity.html"><i class="menu-icon icon-bullhorn"></i>News Feed </a>
-                                </li>-->
-
-                                <li class="active left_icon"><a href="Stest.php"><i class="menu-icon icon-inbox"></i>Test <!--<b class="label green pull-right">
-                                    11</b>--> </a></li>
-
-                                <!--<li><a href="task.html"><i class="menu-icon icon-tasks"></i>Tasks <b class="label orange pull-right">
-                                    19</b> </a></li>-->
-
-                            <!--/.widget-nav-->
-
-
-
-                                <li class="left_icon"><a href="previousresults.php"><i class="menu-icon icon-file"></i>Results </a></li>
-								                        <li class="left_icon"><a href="certificates.php"><i class="menu-icon icon-certificate"></i>Certificates </a></li>
-								                                <li class="left_icon"><a href="index.php"><i class="menu-icon icon-signou"></i>Logout </a></li>
-                                <!--<li><a href="ui-typography.html"><i class="menu-icon icon-book"></i>Typography </a></li>
-                                <li><a href="form.html"><i class="menu-icon icon-paste"></i>Forms </a></li>
-                                <li><a href="table.html"><i class="menu-icon icon-table"></i>Tables </a></li>
-                                <li><a href="charts.html"><i class="menu-icon icon-bar-chart"></i>Charts </a></li>-->
-							</ul>
-                            <!--/.widget-nav-->
-                            <!--<ul class="widget widget-menu unstyled">
-                                <li><a class="collapsed" data-toggle="collapse" href="#togglePages"><i class="menu-icon icon-cog">
-                                </i><i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right">
-                                </i>More Pages </a>
-                                    <ul id="togglePages" class="collapse unstyled">
-                                        <li><a href="other-login.html"><i class="icon-inbox"></i>Login </a></li>
-                                        <li><a href="ot her-user-profile.html"><i class="icon-inbox"></i>Profile </a></li>
-                                        <li><a href="other-user-listing.html"><i class="icon-inbox"></i>All Users </a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#"><i class="menu-icon icon-signout"></i>Logout </a></li>
-                            </ul>-->
-                        </div>
-                        <!--/.sidebar-->
-                    </div>
-	
     <div class="container-fluid">
+
+          	<div class="row">
+
+              <div class="col-md-3">
+                <div class="span3">
+                                     <div class="sidebar" style="display: inline">
+                                         <ul class="widget widget-menu unstyled">
+                                             <li class="left_icon"><a href="dashboard1.php"><i class="menu-icon icon-dashboard"></i>Dashboard
+                                             </a></li>
+                                             <!--<li><a href="activity.html"><i class="menu-icon icon-bullhorn"></i>News Feed </a>
+                                             </li>-->
+
+                                             <li class="active left_icon"><a href="Stest.php"><i class="menu-icon icon-inbox"></i>Test <!--<b class="label green pull-right">
+                                                 11</b>--> </a></li>
+
+                                             <!--<li><a href="task.html"><i class="menu-icon icon-tasks"></i>Tasks <b class="label orange pull-right">
+                                                 19</b> </a></li>-->
+
+                                         <!--/.widget-nav-->
+
+
+
+                                             <li class="left_icon"><a href="previousresults.php"><i class="menu-icon icon-file"></i>Results </a></li>
+             								                        <li class="left_icon"><a href="certificates.php"><i class="menu-icon icon-certificate"></i>Certificates </a></li>
+             								                                <li class="left_icon"><a href="index.php"><i class="menu-icon icon-signou"></i>Logout </a></li>
+                                             <!--<li><a href="ui-typography.html"><i class="menu-icon icon-book"></i>Typography </a></li>
+                                             <li><a href="form.html"><i class="menu-icon icon-paste"></i>Forms </a></li>
+                                             <li><a href="table.html"><i class="menu-icon icon-table"></i>Tables </a></li>
+                                             <li><a href="charts.html"><i class="menu-icon icon-bar-chart"></i>Charts </a></li>-->
+             							</ul>
+                                         <!--/.widget-nav-->
+                                         <!--<ul class="widget widget-menu unstyled">
+                                             <li><a class="collapsed" data-toggle="collapse" href="#togglePages"><i class="menu-icon icon-cog">
+                                             </i><i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right">
+                                             </i>More Pages </a>
+                                                 <ul id="togglePages" class="collapse unstyled">
+                                                     <li><a href="other-login.html"><i class="icon-inbox"></i>Login </a></li>
+                                                     <li><a href="ot her-user-profile.html"><i class="icon-inbox"></i>Profile </a></li>
+                                                     <li><a href="other-user-listing.html"><i class="icon-inbox"></i>All Users </a></li>
+                                                 </ul>
+                                             </li>
+                                             <li><a href="#"><i class="menu-icon icon-signout"></i>Logout </a></li>
+                                         </ul>-->
+                                     </div>
+                                     <!--/.sidebar-->
+                                 </div>
+              </div>
+              	<div class="col-md-8">
 
       <?php
     if(isset($_POST['test']))
@@ -109,8 +124,10 @@ include_once('database.php');
               ?>
               <center class="result_display">
             <?php  echo "<h2>Course name:  $courseName </h2><br>";
-              echo " <h2> Test name : $key </h2>";?>
+              echo " <h2> Test name : $key </h2> ";?>
+
               </center>
+            <?php  echo " <br> ";?>
 <?php
               $fetchqry = "SELECT *
               FROM question
@@ -124,9 +141,6 @@ include_once('database.php');
 
        ?>
 
-    	<div class="row">
-       	<div class="col-md-2"></div>
-        	<div class="col-md-8">
 
 <?php
           $questionNum = 1;
@@ -159,7 +173,7 @@ include_once('database.php');
            <button class="button" name="submit" style="vertical-align:middle"> <span> SUBMIT </span> </button>
 
 				</form></div>
-            <div class="col-md-2"></div>
+
         </div>
     </div>
 
