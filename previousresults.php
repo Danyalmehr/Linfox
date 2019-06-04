87a3828f3e0005c30d11d5a3bd63aec69883cf2a
Amir-89 <45339407+Amir-89@users.noreply.github.com>
2019-06-04 21:57:05 +1000

    Update previousresults.php

--- previousresults.php
+++ previousresults.php
@@ -43,7 +43,7 @@ include_once('include/database.php');
                                 <!--<li><a href="activity.html"><i class="menu-icon icon-bullhorn"></i>News Feed </a>
                                 </li>-->
 
-                                <li class="left_icon"><a href="message.html"><i class="menu-icon icon-inbox"></i>Inbox <!--<b class="label green pull-right">
+                                <li class="left_icon"><a href="message.html"><i class="menu-icon icon-inbox"></i>Test <!--<b class="label green pull-right">
                                     11</b>--> </a></li>
 
                                 <!--<li><a href="task.html"><i class="menu-icon icon-tasks"></i>Tasks <b class="label orange pull-right">
