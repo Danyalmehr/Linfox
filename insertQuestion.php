5d664030973d0d7a11829d51552b05b78f7851ce
Danyalmehr <38877684+Danyalmehr@users.noreply.github.com>
2019-07-26 01:16:43 +1000

    Update insertQuestion.php

--- insertQuestion.php
+++ insertQuestion.php
@@ -1,25 +1,20 @@
 <?php
-session_start();
 include_once('database.php');
 
-  if(!empty($_POST['data']))
-        {
-          $data = $_POST['data'];
-
-          print_r($data);// this displays all the input but i donno how to separate them
-
-          for ($i=0; $i < 6 ; $i++) {
-            $insertqry = "INSERT INTO question (`que`, `option 1`, `option 2`, `option 3`, `option 4`, `ans`) values ('$data','$op1', '$op2', '$op3', '$op4', '$answer')";
-            $result = mysqli_query($con,$insertqry);
-          }
-
-
-          echo " ";
-        }
-
-        else {
-          echo "something is empty";
-        }
-
-
-        ?>
+ if(isset($_POST['submit'])){
+     $fetchqry = "SELECT * FROM `question`";
+     $result=mysqli_query($con,$fetchqry);
+     $num=mysqli_num_rows($result);
+     @$id = $num + 1;
+     @$que = $_POST['question'];
+     @$ans = $_POST['correct_answer'];
+     @$wans1 = $_POST['wrong_answer1'];
+     @$wans2 = $_POST['wrong_answer2'];
+     @$wans3 = $_POST['wrong_answer3'];
+     $qry = "INSERT INTO `question`(`que`, `option 1`, `option 2`, `option 3`, `option 4`, `ans`) VALUES ('$que','$ans','$wans1','$wans2','$wans3','$ans')";
+     $done = mysqli_query($con,$qry);
+     if($done==TRUE){
+     	echo "when the questions are added the list of the questions should be shown here";
+     }
+     	 }
+  ?>
