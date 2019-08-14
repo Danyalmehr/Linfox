23c084e46f83452bdca9ddae65eb28541ca9d981
Amir-89 <amirarh6@gmail.com>
2019-08-14 23:00:30 +1000

    Update Stest.php

--- Stest.php
+++ Stest.php
@@ -209,27 +209,7 @@ include_once('database.php');
 	<?php include("include/footer.inc") ?>
 	
 	
-		<script>
-	
-	// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
 
-// Get the navbar
-var navbar = document.getElementsByClassName("span3");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-	
-	</script>
 
 
   </body>
