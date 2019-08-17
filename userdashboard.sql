5facede5097b8829b3345abcb09914b40ca280f4
Danyalmehr <38877684+Danyalmehr@users.noreply.github.com>
2019-08-17 22:59:19 +1000

    Update userdashboard.sql

--- userdashboard.sql
+++ userdashboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 06, 2019 at 04:19 PM
+-- Generation Time: Aug 17, 2019 at 02:58 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.4
 
@@ -34,19 +34,36 @@ CREATE TABLE `attempt` (
   `att_number` int(10) NOT NULL,
   `test_id` int(255) UNSIGNED DEFAULT NULL,
   `user_id` int(255) UNSIGNED DEFAULT NULL,
-  `att_date` varchar(30) NOT NULL
+  `att_date` varchar(30) NOT NULL,
+  `att_status` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `attempt`
 --
 
-INSERT INTO `attempt` (`att_id`, `final_score`, `att_number`, `test_id`, `user_id`, `att_date`) VALUES
-(251, '50', 1, 2, 1, '2019-06-19 02:00:10am'),
-(252, '50', 2, 2, 1, '2019-06-19 02:00:56am'),
-(255, '0', 1, 3, 1, '2019-07-18 09:05:35pm'),
-(260, '0', 3, 3, 1, '2019-07-28 03:33:30pm'),
-(261, '0', 3, 2, 1, '2019-08-06 12:20:37am');
+INSERT INTO `attempt` (`att_id`, `final_score`, `att_number`, `test_id`, `user_id`, `att_date`, `att_status`) VALUES
+(34, '25', 1, 2, 2, '2019-08-12 07:36:03pm', 'FAIL'),
+(35, '100', 2, 2, 2, '2019-08-12 07:36:23pm', 'PASS'),
+(36, '100', 3, 2, 2, '2019-08-12 07:57:00pm', 'PASS'),
+(37, '50', 4, 2, 2, '2019-08-12 07:59:12pm', 'PASS'),
+(38, '0', 5, 2, 2, '2019-08-12 08:01:33pm', 'Fail'),
+(39, '0', 6, 2, 2, '2019-08-12 08:04:11pm', 'FAIL'),
+(40, '50', 7, 2, 2, '2019-08-12 08:05:52pm', 'PASS'),
+(41, '0', 1, 11, 2, '2019-08-12 08:10:18pm', 'FAIL'),
+(42, '33.333333333333', 8, 2, 2, '2019-08-12 08:12:14pm', 'FAIL'),
+(43, '0', 9, 2, 2, '2019-08-12 08:12:27pm', 'FAIL'),
+(44, '33.333333333333', 10, 2, 2, '2019-08-12 08:20:08pm', 'FAIL'),
+(45, '0', 1, 3, 2, '2019-08-14 09:12:37pm', 'Fail'),
+(46, '0', 1, 13, 2, '2019-08-14 09:17:41pm', 'Fail'),
+(47, '0', 2, 13, 2, '2019-08-14 09:30:00pm', 'Fail'),
+(48, '0', 3, 13, 2, '2019-08-14 09:43:26pm', 'Fail'),
+(49, '0', 1, 2, 8, '2019-08-17 09:04:48pm', 'FAIL'),
+(51, '0', 2, 2, 8, '2019-08-17 10:25:01pm', 'FAIL'),
+(52, '0', 3, 2, 8, '2019-08-17 10:25:21pm', 'FAIL'),
+(53, '0', 4, 2, 8, '2019-08-17 10:26:16pm', 'FAIL'),
+(54, '33.333333333333', 5, 2, 8, '2019-08-17 10:34:10pm', 'FAIL'),
+(55, '33.333333333333', 6, 2, 8, '2019-08-17 10:53:31pm', 'FAIL');
 
 -- --------------------------------------------------------
 
@@ -57,24 +74,16 @@ INSERT INTO `attempt` (`att_id`, `final_score`, `att_number`, `test_id`, `user_i
 CREATE TABLE `courses` (
   `course_id` int(255) UNSIGNED NOT NULL,
   `course_name` text NOT NULL,
-  `course_desc` text NOT NULL,
-  `course_video` varchar(255) DEFAULT NULL
+  `course_desc` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `courses`
 --
 
-INSERT INTO `courses` (`course_id`, `course_name`, `course_desc`, `course_video`) VALUES
-(2, 'course2', '', NULL),
-(3, 'Finalcheck', 'Nothing much', NULL),
-(4, 'Finalcheck', 'asd', NULL),
-(5, 'final', 'Nothing much', NULL),
-(6, 'final', 'Nothing much', NULL),
-(7, 'dasd', 'sdadasd', NULL),
-(8, 'dd', 'ddd', NULL),
-(9, 'dd', 'ddd', NULL),
-(10, 'dd', 'ddd', NULL);
+INSERT INTO `courses` (`course_id`, `course_name`, `course_desc`) VALUES
+(2, 'Laser tag', ''),
+(3, 'Parkour', 'Nothing much');
 
 -- --------------------------------------------------------
 
@@ -99,12 +108,9 @@ CREATE TABLE `question` (
 
 INSERT INTO `question` (`que_id`, `que`, `option 1`, `option 2`, `option 3`, `option 4`, `ans`, `test_id`) VALUES
 (6, 'What is the best helmet brand?', 'aqua', 'red', 'mountain', 'glips', 'mountain', 2),
-(7, 'What\'s your fav fruit?', 'apple', 'cherry', 'orange', 'banana', 'cherry', 2),
-(873, '[1]', '[1]', '[1]', '[1]', '[1]', '[1]', NULL),
-(874, '[2]', '[2]', '[2]', '[2]', '[2]', '[2]', NULL),
-(875, '[3]', '[3]', '[3]', '[3]', '[3]', '[3]', NULL),
-(876, '[4]', '[4]', '[4]', '[4]', '[4]', '[4]', NULL),
-(877, '[5]', '[5]', '[5]', '[5]', '[5]', '[5]', NULL);
+(917, 'd', ' asd asd ', 'asd', 'tyutyuert', 'ddds', 'ddds', 11),
+(926, 'what\'s your name?', ' asd asd ', 'shesh', 'how\'r', 'dan', 'dan', 2),
+(927, 'waht\'s', ' asd asd ', 'asd', 'tyutyuert', 'ddds', 'ddds', 2);
 
 -- --------------------------------------------------------
 
@@ -116,16 +122,18 @@ CREATE TABLE `test` (
   `test_id` int(255) UNSIGNED NOT NULL,
   `test_name` varchar(255) NOT NULL,
   `course_id` int(255) UNSIGNED DEFAULT NULL,
-  `file` text
+  `test_video` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `test`
 --
 
-INSERT INTO `test` (`test_id`, `test_name`, `course_id`, `file`) VALUES
-(2, 'Something else', 2, NULL),
-(3, 'coding challenge', 3, NULL);
+INSERT INTO `test` (`test_id`, `test_name`, `course_id`, `test_video`) VALUES
+(2, 'Safety', 2, 'UFetGppb7Eg'),
+(3, 'coding challenge', 3, NULL),
+(11, 'as', 3, NULL),
+(13, 'Starting the game', 2, NULL);
 
 -- --------------------------------------------------------
 
@@ -147,9 +155,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `fname`, `lname`, `password`, `email`, `user_type`) VALUES
-(1, 'dan', 'dan', '12345678', 'dan@dan', 'employee'),
-(2, 's', 's', '12345678', 's@s.s', 'admin'),
-(3, 'shesh', 'sonar', 'sheshbhushan', 'shesh@s.s', 'admin');
+(2, 's', 's', '12345678', 's@s.s', 'Admin'),
+(3, 'shesh', 'sonar', 'sheshbhushan', 'shesh@s.s', 'Admin'),
+(7, 'dan', 'wqe', '12345678', 'abc@sad', 'Employee'),
+(8, 'dan', 'dan', '123456789', 'dan@dan', 'Employee'),
+(11, 'f', 'f', '12345678', 'f@f', 'Employee');
 
 -- --------------------------------------------------------
 
@@ -164,6 +174,27 @@ CREATE TABLE `useranswer` (
   `user_id` int(255) UNSIGNED DEFAULT NULL,
   `test_id` int(255) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `useranswer`
+--
+
+INSERT INTO `useranswer` (`ans_id`, `userans`, `que_id`, `user_id`, `test_id`) VALUES
+(109, 'mountain', 6, 2, 2),
+(110, 'orange', 6, 2, 2),
+(111, 'dd', 6, 2, 2),
+(112, 'glips', 6, 8, 2),
+(113, ' asd asd ', 6, 8, 2),
+(114, 'tyutyuert', 6, 8, 2),
+(115, 'red', 6, 8, 2),
+(116, ' asd asd ', 6, 8, 2),
+(117, 'ddds', 6, 8, 2),
+(118, 'red', 6, 8, 2),
+(119, ' asd asd ', 6, 8, 2),
+(120, 'ddds', 6, 8, 2),
+(121, 'aqua', 6, 8, 2),
+(122, 'dan', 6, 8, 2),
+(123, 'tyutyuert', 6, 8, 2);
 
 --
 -- Indexes for dumped tables
@@ -221,37 +252,37 @@ ALTER TABLE `useranswer`
 -- AUTO_INCREMENT for table `attempt`
 --
 ALTER TABLE `attempt`
-  MODIFY `att_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=262;
+  MODIFY `att_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `course_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `course_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `que_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=878;
+  MODIFY `que_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=928;
 
 --
 -- AUTO_INCREMENT for table `test`
 --
 ALTER TABLE `test`
-  MODIFY `test_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `test_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `useranswer`
 --
 ALTER TABLE `useranswer`
-  MODIFY `ans_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `ans_id` int(255) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
 
 --
 -- Constraints for dumped tables
