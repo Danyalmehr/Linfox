c058590fba81732a786e1c39589afbdbb666bb8a
sheshsonar <38877666+sheshsonar@users.noreply.github.com>
2019-08-21 15:53:29 +1000

    Previous result is perfect now

--- previousresults.php
+++ previousresults.php
@@ -1,18 +1,17 @@
 <?php
 
 
-//must appear BEFORE the <html> tag
+require 'database.php';
 session_start();
-include_once('database.php');
-?>
+//echo "successful i";?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-      <meta charset="UTF-8">
+<head><meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/responsive.css">
-      <link rel="stylesheet" href="css/test.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -20,184 +19,81 @@ include_once('database.php');
     <script src="js/nav.js"></script>
     <script src="js/read_more.js"></script>
 
-	 <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
         <link type="text/css" href="css/theme.css" rel="stylesheet">
         <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
         <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
 
-    <title>Previous Results</title>
+    <title>Dashboard</title>
+
 </head>
-<style>
-	
-	
-	
-.table_heading {font-size: 30px;padding: 3%;}
-.result_banner{width: 100%;background-color: #E8E1E1;font-size: 1.2em; font-weight: 500;padding: 3%;border-radius: 3%;margin-top:4%;}
-	
-	
-	@media only screen and (max-width: 768px) and (min-width: 428px) {
-		
-		h1, h2{
-				font-size: 18px;
-			}
-		
-		body .admin-side{
-			width: 100%;
-			display: inline;
-		}
-					
-					
-					.sidebar {
-    width: 10%;
-    height: 90%;
-    position: relative;
-			display: inline;
-			
-			
-  }
- li a {float:left;
-	height: 1%;
-	 border-width: 0;
-		
-	  margin-left: 0.15em;
-	 font-size: 9px;
-		}
-		
-		i {
-			float:left;
-			
-		}
-
-
-
-		.results{
-			margin-left: 25em;
-		}
-		
-		
-		}
-		
-		@media only screen and (max-width: 428px) {
-			
-			h1, h2{
-				font-size: 18px;
-			}
-		
-		body .admin-side{
-			width: 100%;
-			display: inline;
-		}
-			
-		.sidebar {
-    width: 10%;
-    height: 90%;
-    position: relative;
-			display: inline;
-			
-			
-  }
- li a {float:left;
-	height: 1%;
-	 border-width: 0;
-		
-	  margin-left: 0.05em;
-	 font-size: 7px;
-		}
-		
-		i {
-			float:left;
-			
-		}
-		}
-	
-	
-</style>
 <body onLoad="run_first()">
 	<?php include("include/banner.inc") ?>
     <?php include("include/nav.inc") ?>
 
     <div class="container-fluid">
-      
-		<div class="sticky-top">
-                      <div class="span3">
-                          <div class="sidebar" style="display: inline">
-                              <ul class="widget widget-menu unstyled">
-                                  <li class="left_icon"><a href="dashboard.php"><i class="menu-icon icon-dashboard"></i>Dashboard
-                                  </a></li>
-                                  <!--<li><a href="activity.html"><i class="menu-icon icon-bullhorn"></i>News Feed </a>
-                                  </li>-->
-
-                                  <li><a href="Stest.php"><i class="menu-icon icon-inbox"></i>Test <!--<b class="label green pull-right">
-                                      11</b>--> </a></li>
-
-
-                                  <li class="active"><a href="previousresults.php"><i class="menu-icon icon-file"></i>Results </a></li>
-                                          <li><a href="certificates.php"><i class="menu-icon icon-certificate"></i>Certificates </a></li>
-                                                  <li><a href="index.php"><i class="menu-icon icon-signou"></i>Logout </a></li>
-
-                </ul>
-
-                          </div>
-                          <!--/.sidebar-->
-                      </div>
-	
-	</div>
-		
+      <?php include("admin-side-dash.html") ?>
     	<div class="row">
 
+                    <div class="col-md-12 col-md-9">
+                          <center> <h1>Edit User Details</h1></center>
 
-    <div class="col-md-9">
+                      			<?php $fetchqry = "SELECT final_score, test_name, fname, lname, course_name, att_date, att_status, att_number
+                              FROM attempt
+                              INNER JOIN test ON test.test_id = attempt.test_id
+                              INNER JOIN user ON user.user_id = attempt.user_id
+                              INNER JOIN courses ON courses.course_id = test.course_id
+                              WHERE email = '$email'";
+                                $result=mysqli_query($con,$fetchqry);
+                      			 ?>
+                             <div class="search_results" id="search_results">
 
-<?php
-        $fetchqry = "SELECT final_score, test_name, fname, lname, course_name, att_date, att_status, att_number
-        FROM attempt
-        INNER JOIN test ON test.test_id = attempt.test_id
-        INNER JOIN user ON user.user_id = attempt.user_id
-        INNER JOIN courses ON courses.course_id = test.course_id
-        WHERE email = '$email'
-        ";
-        $result=mysqli_query($con,$fetchqry);
-        while ($row = mysqli_fetch_array($result)) {
+                          	<table class="search_table" id="search_table" >
+                              <tr><center class="table_heading">Test results for  <?php echo" $fname "?> are as following<Center></tr><br><br>
 
-        $final_score = $row['final_score'];
-        $testName = $row['test_name'];
-        $courseName = $row['course_name'];
-        $attemptDate = $row['att_date'];
-        $att_status = $row['att_status'];
-        $att_number = $row['att_number'];
+                      			<?php
 
-        ?>
-        <div class="search_results" id="search_results">
-                <table id="search_table">
-                  <tr><center class="table_heading">Test results for  <?php echo" $fname "?> are as following<Center></tr>
-                   <tr>
-                        <th>Final score</th>
-                        <th>Course name</th>
-                        <th>Status</th>
-                        <th>Attempt number</th>
-                        <th>Test name</th>
-                        <th>Date</th>
-                  </tr>
+                            echo "<tr>
+                                    <th>Final score</th>
+                                    <th>Course name</th>
+                                    <th>Status</th>
+                                    <th>Attempt number</th>
+                                    <th>Test name</th>
+                                    <th>Date</th>
+                                  </tr>";
+                    			    while($row = mysqli_fetch_array($result)):
+                      				{
+                      				echo "<tr><form action=user-process.php method=post>";
+                              echo "<tr>
+                                        <td><input class='inputwidthforemailandotherinputintable' type=text name=fname value='".$row['final_score']."' </td>
+                                        <td><input class='inputwidthforemailandotherinputintable' type=text name=lname value='".$row['course_name']."'</td>
+                                        <td><input class='inputwidthforemailandotherinputintable' type=email name=email value='".$row['att_status']."'</td>
+                                        <td><input class='inputwidthforemailandotherinputintable'type=text name=password value='".$row['att_number']."'</td>
+                                        <td><input class='inputwidthforemailandotherinputintable'type=text name=user_type value='".$row['test_name']."'</td>
+                                        <td><input class='inputwidthforemailandotherinputintable'type=text name=user_type value='".$row['att_date']."'</td>
+                                    </tr>";
+                        				echo "</form></tr>";
+                      				}
+                                      ?>
+                                      <?php endwhile;?>
+                      </table>
+                         </div>
+                          </div>
 
-
-                	<tr>
-                    <td><?= $final_score ?><?php echo " % "?></td>
-                    <td><?= $courseName ?></td>
-                    <td><?= $att_status ?></td>
-                    <td><?= $att_number ?></td>
-                    <td><?= $testName ?></td>
-                    <td><?= $attemptDate ?></td>
-               		</tr>
-
-            </table>
-     </div>
-
-
-    <?php } ?>
-    </div>
-    </div>
-  </div>
+						</div>
+					</div>
+          </div>
+        </div>
 	<?php include("include/footer.inc") ?>
+  <script type="text/javascript" src="js/confirmation.js"></script>
+
+<!--!	<script>
+		function(popup){
+			var btn=document.getElementsByClassName("btn-course")
+			var
+		}
+	</script>-->
 </body>
 </html>
