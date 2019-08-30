bb2332119bdf61565314bc6f4699b48262f08377
Amir-89 <amirarh6@gmail.com>
2019-08-31 04:50:08 +1000

    sidebar

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,27 @@
 function run_first() {
     if(sessionStorage.item_id) {
-        document.getElementById(sessionStorage.item_id.toString()).classList.add("text-white");
+        document.getElementById(sessionStorage.item_id.toString()).classList.add("active");
     }
     else {
-        document.getElementById("0").classList.add("text-white");
+        document.getElementById("0").classList.add("active");
     }
 }
 
 function nav_item_selected(item_id) {
     for(i = 0; i < 5; i++) {
-        document.getElementById(i.toString()).classList.remove("text-white");
+        document.getElementById(i.toString()).classList.remove("active");
     }
-    document.getElementById(item_id.toString()).classList.add("text-white");
+    document.getElementById(item_id.toString()).classList.add("active");
 
     //store item_id in sessionStorage
     sessionStorage.item_id = item_id;
+}
+
+function run_first() {
+    if(sessionStorage.item_id) {
+        document.getElementById(sessionStorage.item_id.toString()).classList.add("active");
+    }
+    else {
+        document.getElementById("0").classList.add("active");
+    }
 }
