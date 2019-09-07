1396517b82f265fa31efa80efa118f0a9fb8b8de
Danyalmehr <38877684+Danyalmehr@users.noreply.github.com>
2019-09-08 00:13:47 +1000

    Add files via upload

---
+++ mark-answers-course.php
@@ -0,0 +1,103 @@
+<?php
+if(!isset($_SERVER['HTTP_REFERER'])){
+    // redirect them to your desired location
+    header('location: index.php');
+    exit;
+}
+//must appear BEFORE the <html> tag
+session_start();
+include_once('database.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/test.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+      <link type="text/css" href="css/theme.css" rel="stylesheet">
+    <script src="js/nav.js"></script>
+    <script src="js/read_more.js"></script>
+
+        <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+        <link type="text/css" href="css/theme.css" rel="stylesheet">
+        <link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
+        <link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
+
+
+
+    <link type="text/css" href="css/theme.css" rel="stylesheet">
+
+    <title> Take test </title>
+
+	<style>
+
+  ul.unstyled, ol.unstyled {
+     margin-left: 0;
+     list-style: none;
+}
+		.span3 {
+
+			margin-right: 4em;
+		}
+    .widget-menu {
+    background: #fff;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+     border-radius: 3px;
+    overflow: hidden;
+}
+
+	</style>
+</head>
+<body onLoad="run_first()">
+	<?php include("include/banner.inc") ?>
+    <?php include("include/nav.inc") ?>
+
+    <div class="container-fluid">
+      <?php include("admin-side-dash.html") ?>
+      <div class="row">
+          <div class="col-md-offset-2 col-md-8">
+            <?php
+
+            $courses = "SELECT *
+                      FROM courses
+                      ";
+            					$result = mysqli_query($con,$courses);
+                      ?>
+                      <h1 style="float: auto;"> To check on user you need to choose the course first </h1>
+                      <h3>STEP 1: Choose your COURSE</h3>
+
+
+
+                      <?php
+            					while ($row=mysqli_fetch_array($result))
+                          {
+                            $course_name=$row['course_name'];
+                            $course_id=$row['course_id'];
+                            $course_desc=$row['course_desc'];?>
+
+      <a href="mark-answers.php?id=<?=$course_id?>&name= <?= htmlentities($course_name) ?>"><button class="btn btn-secondary btn-lg span5 btn-course" name="selectedtest" style="float: auto;"> <?= $course_name  ?></button></a>
+
+
+              <?php } ?>
+   </div>
+ </div>
+</div>
+
+
+
+
+
+	<?php include("include/footer.inc") ?>
+
+
+  </body>
+</html>
