96847755db0ca1d8192ce0da7b44892960ab7670
Danyalmehr <38877684+Danyalmehr@users.noreply.github.com>
2019-09-25 23:08:55 +1000

    admindashboard
    working on admin dashboard

--- admindashboard.php
+++ admindashboard.php
@@ -122,7 +122,21 @@ require 'database.php';
 
 		}
 
-
+    .user-process{
+    border: 1.2px solid black;
+    border-radius: 50%;
+    height:80px;
+    width: 80px;
+    font-size: 25px;
+    font-weight: bolder;
+    font-family: sans-serif;
+    text-align: center;
+    margin-bottom: 6px;
+    left: 50px;
+    vertical-align: middle;
+    line-height: 70px;
+    margin: 19px 15px;
+    }
 
 	</style>
 </head>
@@ -132,6 +146,27 @@ require 'database.php';
 
     <div class="container-fluid">
       <?php include("admin-side-dash.html") ?>
+      <?php
+      $fetchqry1 = "SELECT count(ans_id) AS countOfAnsId
+                   FROM useranswer
+                   INNER JOIN question ON question.que_id = useranswer.que_id
+                   WHERE ans_status IS NULL and que_type= 'shortans'
+                    ";
+      $result1=mysqli_query($con,$fetchqry1);
+      $row1=mysqli_fetch_array($result1);
+
+      $fetchqry = "SELECT count(Distinct test_id) AS countOfTestId, count(Distinct user_id) AS countOfUserId
+                   FROM useranswer
+                   WHERE ans_status IS NULL
+                    ";
+      $result=mysqli_query($con,$fetchqry);
+
+      $count_ans = $row1['countOfAnsId'];
+      $count_test = $row['countOfTestId'];
+      $count_user = $row['countOfUserId'];
+
+
+       ?>
 
     	<div class="row">
 
@@ -140,6 +175,27 @@ require 'database.php';
                             <div class="btn-controls">
                                 <div class="btn-box-row row-fluid course">
 					<h1 style="margin: 1em"> Admin Dashboard</h1>
+
+          <div class="col-sm-2 colum2-user-process">
+            <div class=" user-process">
+            <?= $count_ans ?>
+            </div>
+              Total number of unmarked ANSWERS
+          </div>
+
+          <div class="col-sm-2 colum2-user-process">
+            <div class=" user-process">
+            <?= $count_test ?>
+            </div>
+              Total number of unmarked TESTS
+          </div>
+
+          <div class="col-sm-2 colum2-user-process">
+            <div class=" user-process">
+            <?= $count_user ?>
+            </div>
+              Total number of unmarked USERS
+          </div>
 
 
 
@@ -151,6 +207,70 @@ require 'database.php';
 						</div>
 					</div>
           </div>
+
+
+
+          <div class="row">
+
+                        <div class="col-md-12">
+                            <div class="content">
+                                <div class="btn-controls">
+                                    <div class="btn-box-row row-fluid course">
+              <h1 style="margin: 1em"> Shortcuts</h1>
+
+    <a href="check-on-user.php"><button class="btn btn-secondary btn-lg span5 btn-course" name="selectedtest" style="float: auto;"> User Progress</button></a>
+    <a href="question-course.php"><button class="btn btn-secondary btn-lg span5 btn-course" name="selectedtest" style="float: auto;"> Create Question</button></a>
+    <a href="create-course.php"><button class="btn btn-secondary btn-lg span5 btn-course" name="selectedtest" style="float: auto;"> Create Course</button></a>
+    <a href="check-on-user.php"><button class="btn btn-secondary btn-lg span5 btn-course" name="selectedtest" style="float: auto;"> Create Test</button></a>
+  </div>
+</div>
+
+</div>
+</div>
+</div>
+
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart()
+    {
+         var data = google.visualization.arrayToDataTable([
+                   ['test_id', 'user_id'],
+                   <?php
+                   while($row = mysqli_fetch_array($result))
+                   {
+                        echo "['".$row["countOfTestId"]."', '".$row["countOfUserId"]."'],";
+                   }
+                   ?>
+              ]);
+         var options = {
+               title: 'Percentage of Male and Female Employee',
+               //is3D:true,
+               pieHole: 0.4
+              };
+         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+         chart.draw(data, options);
+    }
+    </script>
+
+    <div class="row">
+
+      <br /><br />
+    <div style="width:900px;">
+         <h3 align="center">Make Simple Pie Chart by Google Chart API with PHP Mysql</h3>
+         <br />
+         <div id="piechart" style="width: 900px; height: 500px;"></div>
+    </div>
+  </div>
+
+
+
+
+
+
+
         </div>
 	<?php  include("include/footer.inc") ?>
 
